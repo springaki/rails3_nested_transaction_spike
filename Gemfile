c5aa01447535a92771c6deffc0c47f480385237e
Aki <aki.onetime@gmail.com>
2016-09-21 17:17:49 +0900

    add example spec

--- Gemfile
+++ Gemfile
@@ -53,8 +53,9 @@ end
 group :test do
   # gem "faker", "~> 1.4.3"
   gem "capybara", "~> 2.4.3"
-  gem "database_cleaner", "~> 1.3.0"
+  gem 'database_rewinder'
   # gem "launchy", "~> 2.4.2"
   # gem "selenium-webdriver", "~> 2.43.0"
+
 end
 
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,56 @@
 require 'rails_helper'
 
-RSpec.describe Question, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Question, :type => :model do
+
+  describe "シンプルなトランザクションの場合" do
+
+    example "普通の書き方でロールバックされる" do
+      begin
+        ActiveRecord::Base.transaction do
+          Question.create
+          raise ActiveRecord::Rollback
+        end
+      rescue
+        raise # ココには入りません
+      end
+      expect(Question.count).to eq 0
+    end
+
+  end
+
+
+  describe "入れ子のトランザクションの場合" do
+
+    example "普通の書き方だとすべてロールバックされない" do
+      begin
+        ActiveRecord::Base.transaction do
+          Question.create
+          ActiveRecord::Base.transaction do
+            Question.create
+            raise ActiveRecord::Rollback
+          end
+        end
+      rescue
+        raise # ココには入りません
+      end
+      expect(Question.count).to eq 2
+    end
+
+    example "requires_new を付けると想定通り内側だけロールバックされる" do
+      begin
+        ActiveRecord::Base.transaction do
+          Question.create
+          ActiveRecord::Base.transaction(requires_new: true) do
+            Question.create
+            raise ActiveRecord::Rollback
+          end
+        end
+      rescue
+        raise # ココには入りません
+      end
+      expect(Question.count).to eq 1
+    end
+
+  end
+
 end
